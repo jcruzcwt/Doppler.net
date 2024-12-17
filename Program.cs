ede7f6ef4633a163343736dacfe0a71c40e3214d
JCruz <U135JXC@mycwt.com>
2024-12-17 09:30:22 +0800

    Veracode MPT - Dec 2024

--- Program.cs
+++ Program.cs
@@ -24,19 +24,24 @@ namespace LoGiC.NET
 
         static void Main(string[] args)
         {
-            //args[9] = "C:\\CWTProjects\\bpg-cbt-power-express\\PL_Express\\bin\\x86\\Debug\\PowerExpress.exe";
-            //args[1] = "C:\\CWTProjects\\bpg-cbt-power-express\\PL_Express\\Obfuscator\\Processed\\";
-
             string path = args[0];
             string pathOutput = args[1] + Path.GetFileName(path);
 
-            Console.WriteLine("Obfuscate Start:\nFILE : " + path);
-            Console.WriteLine("OUTPUT : " + pathOutput);
+            System.Collections.Generic.List<string> customObfuscation = new System.Collections.Generic.List<string>();
+            //customObfuscation.Add("DAL_DataAccess.dll");
 
-            Console.WriteLine("Preparing obfuscation...");
+            customObfuscation.ForEach(s => s = s.ToUpper());
+
+            bool isCustomObfuscation = customObfuscation.Contains(Path.GetFileName(path).ToUpper());
+            Console.WriteLine($"Custom Obfuscation: -- " + (isCustomObfuscation ? "Y" : "N"));
+
+            Console.WriteLine($"Obfuscate Start:\nFILE : " + path);
+            Console.WriteLine($"OUTPUT : " + pathOutput);
+
+            Console.WriteLine($"Preparing obfuscation...");
             if (!File.Exists("config.txt"))
             {
-                Console.WriteLine("Config file not found, continuing without it.");
+                Console.WriteLine($"Config file not found, continuing without it.");
                 goto obfuscation;
             }
             Parser p = new Parser() { ConfigFile = "config.txt" };
@@ -52,48 +57,70 @@ namespace LoGiC.NET
 
             FilePath = path;
 
-            Console.WriteLine("Renaming...");
+            Console.WriteLine($"Renaming...");
             Renamer.Execute();
+            Console.WriteLine($"   Done");
 
-            Console.WriteLine("Adding junk methods...");
+            Console.WriteLine($"Adding junk methods...");
             JunkMethods.Execute();
+            Console.WriteLine($"   Done");
 
             //Console.WriteLine("Adding proxy calls...");
             //ProxyAdder.Execute(); // SIRA
 
-            Console.WriteLine("Encrypting strings...");
+            Console.WriteLine($"Encrypting strings...");
             StringEncryption.Execute();
+            Console.WriteLine($"   Done");
 
-            Console.WriteLine("Injecting Anti-Tamper...");
+            Console.WriteLine($"Injecting Anti-Tamper...");
             AntiTamper.Execute();
+            Console.WriteLine($"   Done");
 
-            Console.WriteLine("Executing Anti-De4dot...");
+            Console.WriteLine($"Executing Anti-De4dot...");
             AntiDe4dot.Execute();
+            Console.WriteLine($"   Done");
 
-            Console.WriteLine("Executing Control Flow...");
+            Console.WriteLine($"Executing Control Flow...");
             ControlFlow.Execute();
+            Console.WriteLine($"   Done");
 
-            Console.WriteLine("Encoding ints...");
-            IntEncoding.Execute(); // partially sira
+            Console.WriteLine($"Encoding ints...");
+            if (!isCustomObfuscation)
+            {
+                IntEncoding.Execute(); // partially sira
+                Console.WriteLine($"   Done");
+            }
+            else
+            {
+                Console.Write($" Skipped");
+            }
 
-            Console.WriteLine("Watermarking...");
+            Console.WriteLine($"Watermarking...");
             Watermark.AddAttribute();
+            Console.WriteLine($"   Done");
 
-            Console.WriteLine("Saving file...");
-            Console.WriteLine("Writing to: " + pathOutput);
+            Console.WriteLine($"Saving file...");
+            Console.WriteLine($"Writing to: " + pathOutput);
 
             FilePath = pathOutput;
             Module.Write(Stream, new dnlib.DotNet.Writer.ModuleWriterOptions(Module) { Logger = DummyLogger.NoThrowInstance });
+            Console.WriteLine($"   Done");
 
+            Console.WriteLine($"Strip DOS Header...");
             StripDOSHeader.Execute();
+            Console.WriteLine($"   Done");
 
             // Save stream to file
+            Console.WriteLine($"Saving file stream...");
             File.WriteAllBytes(pathOutput, Stream.ToArray());
+            Console.WriteLine($"   Done");
 
+            Console.WriteLine($"Anti-Tamper Injection...");
             if (AntiTamper.Tampered)
                 AntiTamper.Inject(FilePath);
+            Console.WriteLine($"   Done");
 
-            Console.WriteLine("Done!");
+            Console.WriteLine($"ALL DONE!");
         }
     }
 }
